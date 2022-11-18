0df7fd842a5d05f79b91f5d0e7b4d4fc5acae622
Abhith Shaji <75518181+Abhith-S@users.noreply.github.com>
2022-11-18 22:43:42 +0530

    Create IfElse.sol

---
+++ IfElse.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+contract ForLoop{
+
+    // a function that uses 'for' loop to take the aggregate sum of all values from 1 upto a limit
+    function forLoopExample(uint _limit)public pure returns(uint){
+
+        uint aggregateSum;
+
+        //For loop
+        for(uint i = 1; i <= _limit; i++){
+            aggregateSum += i;
+        }
+        return aggregateSum;
+
+    }
+}
+
+contract WhileLoop{
+
+    // a function that uses 'while' loop to take the aggregate sum of all values from 1 upto a limit
+    function whileLoopExample(uint _limit)public pure returns(uint){
+
+        uint aggregateSum;
+        uint i = 1;
+
+        //While loop
+        while(i <= _limit){
+            aggregateSum += i;
+            i++;
+        }
+        return aggregateSum;
+
+    }
+}
