f51398cada5f637ecb5622efb8541662c4be6540
Abhith Shaji <75518181+Abhith-S@users.noreply.github.com>
2022-11-18 22:47:00 +0530

    Create pureVsView.sol

---
+++ pureVsView.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract viewVsPure{
+
+    //declaring a state variable
+    uint num = 10;
+
+    //view function example
+    function viewFunction()public view returns(uint){
+        
+        //returning a state varible ,ie , reading data from the blockchain
+        return num;
+    }
+    
+    //view function example2
+    function viewFunction(uint x)public view returns(uint){
+        
+        //returning a state varible ,ie , reading data from the blockchain and adding it to local variable
+        return num + x;
+    }
+
+    //pure function example
+    function pureFunction(uint x , uint y)public pure returns(uint){
+        
+        //returning only local variables , no state variable
+        return x + y;
+    }
+    
+    //pure function example2
+    function pureFunction2()public pure returns(uint){
+        
+        //returning just a vlue
+        return 1;
+    }
+}
