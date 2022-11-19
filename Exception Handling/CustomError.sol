96d017e8c57d8c8c85d725462231fcc1863f40be
Abhith Shaji <75518181+Abhith-S@users.noreply.github.com>
2022-11-19 22:23:56 +0530

    Create CustomError.sol

---
+++ Exception Handling/CustomError.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CustomError{
+
+    uint public count;
+
+    //craete custom error called customError()
+    //it can have input parameters 
+    error customError(address owner, uint x);
+
+    function customErrorFunction(uint _x)public {
+        if(_x > 10){
+            revert customError(msg.sender,_x);
+        }else{
+            count += _x;
+        }
+    }
+}
