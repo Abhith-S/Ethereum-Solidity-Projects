8185909b9887246588ba53300624edd68f94874a
Abhith Shaji <abhithshaji@gmail.com>
2022-12-04 17:07:26 +0530

    Update SharedWallet.sol

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -10,15 +10,31 @@ pragma solidity ^0.8.13;
 //importing contract that sets msg.sender as owner and gives other functionalities
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SharedWallet is Ownable{
+//creating another contract for allowance
+contract Allowance is Ownable{
 
     //mapping to map address to corresponding allowance
     mapping(address => uint) public allowance;
 
-    //fallback function to deposit funds to SC
-    fallback() external payable{}
+    //function to set allowance to addresses
+    //the onlyOwner modifier here comes from openzepplin ownable.sol
+    function setAllowance(address _to, uint  _amount)public onlyOwner{
+        allowance[_to] = _amount;
+    }
 
-    //the fallback fn needs a recieve ether function to get rid of the warnings
+
+    //funtion to reduce allowance
+    function reduceAllowance(address _to, uint _amount)internal{
+
+        allowance[_to] -= _amount;
+    }
+}
+
+
+//contract for shared wallet
+contract SharedWallet is Allowance{
+
+    //as we are not sending any data (in calldata of low level transaction) with the ether we can use recieve()
     receive() external payable{}
 
 
@@ -30,18 +46,16 @@ contract SharedWallet is Ownable{
         _;
     }
 
-    //function to set allowance to addresses
-    //the onlyOwner modifier here comes from openzepplin ownable.sol
-    function setAllowance(address _to, uint  _amount)public onlyOwner{
-        allowance[_to] = _amount;
-    }
-
 
     //withdraw by allowance allowed or any amount by owner
     //can pass the _amount from withdraw() to ownerOrAllowed()
     function withdraw(address payable _to,uint _amount)public ownerOrAllowed(_amount){
+        
+        //lets give a custom error message if there is no money im contract
+        require(address(this).balance >= _amount, "Not enough funds in the contract");
 
-        //allowance[_to] -= _amount;
+        //reduce allowance
+        reduceAllowance(msg.sender,_amount);
 
         _to.transfer(_amount);
     }
